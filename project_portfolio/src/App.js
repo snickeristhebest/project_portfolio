7e758ce861e501180287cd791b40b2aab48e0a1f
snickeristhebest <nicoh7242@gmail.com>
2025-11-26 00:12:25 -0600

    splash

--- project_portfolio/src/App.js
+++ project_portfolio/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import faceImage from './assets/images/face.jpeg';
+import batmanAudio from './assets/audio/Classic Batman Transition - QuickSounds.com.mp3';
+import { useRef, useState } from 'react';
+import Confetti from 'react-confetti';
 
 function App() {
+  const audioRef = useRef(null);
+  const [started, setStarted] = useState(false);
+
+  const startSite = () => {
+    setStarted(true);
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <audio ref={audioRef} src={batmanAudio} />
+      
+      {!started ? (
+        <div className="start-screen">
+          <button className="start-button" onClick={startSite}>
+            Enter Site
+          </button>
+        </div>
+      ) : (
+      <>
+        <Confetti
+            width={window.innerWidth}
+            height={window.innerHeight}
+            recycle={false}
+            numberOfPieces={500}
+          />
+        <header className="App-header">
+          <img src={faceImage} className="App-logo growing" alt="face" />
+          <script src="https://app.embed.im/confetti.js" defer></script>
+        </header>
+      </>  
+      )}
     </div>
   );
 }
